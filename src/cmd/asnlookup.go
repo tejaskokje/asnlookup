9eb68b9d5d661fe21671565690274d5436c7c594
Tejas Kokje <tejas.kokje@gmail.com>
2019-02-18 01:36:29 -0800

    Adding config test files

--- src/cmd/asnlookup.go
+++ src/cmd/asnlookup.go
@@ -9,73 +9,30 @@ import (
 	"strings"
 )
 
-func getConfig() (string, error) {
-	var reader io.Reader
-
-	configURL := "http://lg01.infra.ring.nlnog.net/table.txt"
-	configFile := os.Getenv("CONFIG_FILE_PATH")
-
-	if configFile == "" {
-		resp, err := http.Get(configURL)
-		if err != nil {
-			return "", err
-		}
-		defer resp.Body.Close()
-		reader = resp.Body
-	} else {
-
-		file, err := os.Open(configFile)
-		if err != nil {
-			return "", err
-		}
-		defer file.Close()
-
-		reader = file
-	}
-
-	text, err := ioutil.ReadAll(reader)
-	if err != nil {
-		return "", err
-	}
-
-	return string(text), nil
-}
-
 func main() {
 
-	cfgStr, err := asnlookup.GetConfig()
+	cfg, err := asnlookup.GetConfig()
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 		os.Exit(1)
 	}
 
-	args := os.Args[1:]
-	numArgs := len(args)
-	if numArgs == 0 {
-		fmt.Println("Please provide IPv4 or IPv6 address for ASN lookup")
-		os.Exit(1)
-	} else if numArgs > 1 {
-		fmt.Println("Please provide only one IP address")
-		os.Exit(1)
-	}
-
-	reqIP := args[0]
 	var newIPAddressFunc func(string, int) (asnlookup.IPAddress, error)
 	var isValidCidrFunc func(string) bool
 	var ipToFind asnlookup.IPAddress
-	if asnlookup.IsValidIPv4(reqIP) {
+	if asnlookup.IsValidIPv4(cfg.IPToFind) {
 		//Check if reqIP is valid IPV4 or v6
-		ipToFind, err = asnlookup.NewIPv4Address(reqIP+"/32", -1)
+		ipToFind, err = asnlookup.NewIPv4Address(cfg.IPToFind+"/32", -1)
 		if err != nil {
-			fmt.Printf("Error encountered for target IP %s: %v\n", reqIP, err)
+			fmt.Printf("Error encountered for target IP %s: %v\n", cfg.IPToFind, err)
 			os.Exit(1)
 		}
 		newIPAddressFunc = asnlookup.NewIPv4Address
 		isValidCidrFunc = asnlookup.IsValidIPv4Cidr
-	} else if asnlookup.IsValidIPv6(reqIP) {
-		ipToFind, err = asnlookup.NewIPv6Address(reqIP+"/128", -1)
+	} else if asnlookup.IsValidIPv6(cfg.IPToFind) {
+		ipToFind, err = asnlookup.NewIPv6Address(cfg.IPToFind+"/128", -1)
 		if err != nil {
-			fmt.Printf("Error encountered for target IP %s: %v\n", reqIP, err)
+			fmt.Printf("Error encountered for target IP %s: %v\n", cfg.IPToFind, err)
 			os.Exit(1)
 		}
 		newIPAddressFunc = asnlookup.NewIPv6Address
@@ -83,12 +40,12 @@ func main() {
 
 	} else {
 		fmt.Println("Invalid IP address in input")
-		os.Exit(0)
+		os.Exit(1)
 	}
 
 	t := asnlookup.NewTrie()
 
-	scanner := bufio.NewScanner(strings.NewReader(cfgStr))
+	scanner := bufio.NewScanner(strings.NewReader(cfg.IPCidrList))
 	for scanner.Scan() {
 		parts := strings.Split(strings.Trim(scanner.Text(), " "), " ")
 		isValidCidr := isValidCidrFunc(parts[0])
@@ -109,7 +66,4 @@ func main() {
 	for _, info := range nodeInfoList {
 		fmt.Printf("%s/%d %d\n", info.Subnet, info.Cidr, info.Asn)
 	}
-
-	//asnlookup.DumpTrie(t)
-
 }
